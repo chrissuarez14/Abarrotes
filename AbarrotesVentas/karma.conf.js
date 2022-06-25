c796d18e3525b86eda1916e57411ec286d9d7be7
chrissuarez14 <chrissuarez14.1.2@gmail.com>
2022-06-25 16:51:33 -0500

    Se modifica proyecto con sass y no con scss

--- AbarrotesVentas/karma.conf.js
+++ AbarrotesVentas/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ventas-abarrotes'),
+      dir: require('path').join(__dirname, './coverage/abarrotes-ventas'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- AbarrotesVentas/src/app/app.component.spec.ts
+++ AbarrotesVentas/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'VentasAbarrotes'`, () => {
+  it(`should have as title 'AbarrotesVentas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VentasAbarrotes');
+    expect(app.title).toEqual('AbarrotesVentas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('VentasAbarrotes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AbarrotesVentas app is running!');
   });
 });
